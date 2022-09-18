c38302c3b29140eb6528e2c4dbb3ae7f70052792
Tejas Kothari <tejas23682320@gmail.com>
2022-09-18 20:22:08 +0800

    Input address to connect

--- lib/widgets/connect_wallet.dart
+++ lib/widgets/connect_wallet.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../app_model.dart';
-import './pera_wallet_connect.dart';
+import '../helper.dart';
 
 class ConnectWallet extends StatefulWidget {
   const ConnectWallet({super.key});
@@ -11,16 +11,40 @@ class ConnectWallet extends StatefulWidget {
 }
 
 class ConnectWalletState extends State<ConnectWallet> {
+  String inputAddress = '';
+
   @override
   Widget build(BuildContext context) {
     return Consumer<AppModel>(builder: (context, model, child) {
       return Row(mainAxisAlignment: MainAxisAlignment.end, children: [
         IconButton(
-            onPressed: () async {
-              var pera = PeraWalletConnect();
-              var uri = await pera.connect(context);
+            onPressed: () {
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: const Text('Input wallet address'),
+                      content: TextField(onChanged: (text) {
+                        inputAddress = text;
+                      }),
+                      actions: <Widget>[
+                        TextButton(
+                            onPressed: () => Navigator.pop(context, 'Cancel'),
+                            child: const Text('Cancel')),
+                        TextButton(
+                            onPressed: () {
+                              Provider.of<AppModel>(context, listen: false)
+                                  .setUserAddress(inputAddress);
+                              Navigator.pop(context, 'OK');
+                            },
+                            child: const Text('OK'))
+                      ],
+                    );
+                  });
             },
-            icon: const Icon(Icons.account_balance_wallet_outlined))
+            icon: const Icon(Icons.account_balance_wallet_outlined)),
+        if (model.userAddress != null)
+          Text(getAbbreviatedAddress(model.userAddress!))
       ]);
     });
   }
